70b681cb81a871b73af204e6df50e8ebcef9f56c
Isaac Vega <skylinecar64@gmail.com>
2023-06-09 15:04:07 -0600

    Fix bugs themes

--- js/temas.js
+++ js/temas.js
@@ -2,6 +2,8 @@ const themeButton = document.getElementById("tema-btn");
 const themeStyle = document.getElementById("estilo-tema");
 const logo = document.getElementById("logo");
 const signature = document.getElementById("signature");
+const elements = document.querySelectorAll("li.skills__img, li.hobbies__img");
+const links = document.querySelectorAll("li.title__network__item a");
 
 themeButton.addEventListener("click", function () {
     themeButton.classList.toggle("active");
@@ -10,9 +12,27 @@ themeButton.addEventListener("click", function () {
         themeStyle.setAttribute("href", "css/tema-oscuro.css");
         logo.setAttribute("src", "assets/general/vector_1_2.png");
         signature.setAttribute("src", "assets/general/signature_2.png");
+        imgFilter("ligth");
     } else {
         themeStyle.setAttribute("href", "css/tema-claro.css");
         logo.setAttribute("src", "assets/general/vector_1_1.png");
         signature.setAttribute("src", "assets/general/signature_1.png");
+        imgFilter("");
     }
 });
+
+function imgFilter(theme) {
+    elements.forEach((element) => {
+        var img = element.querySelector("img");
+        theme === "ligth"
+            ? (img.style.filter = "contrast(0) brightness(2)")
+            : (img.style.filter = "none");
+    });
+
+    links.forEach((link) => {
+        var img = link.querySelector("img");
+        theme === "ligth"
+            ? (img.style.filter = "invert(100%)")
+            : (img.style.filter = "none");
+    });
+}
