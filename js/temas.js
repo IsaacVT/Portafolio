e2dd7dce8401bab5a0c34801e4501ae944b97d60
Isaac Vega <skylinecar64@gmail.com>
2023-06-12 14:28:32 -0600

    Actualizando tema

--- js/temas.js
+++ js/temas.js
@@ -6,8 +6,6 @@ const elements = document.querySelectorAll("li.skills__img, li.hobbies__img");
 const links = document.querySelectorAll("li.title__network__item a");
 
 themeButton.addEventListener("click", () => {
-    themeButton.classList.toggle("active");
-
     if (themeStyle.getAttribute("href") === "css/tema-claro.css") {
         themeStyle.setAttribute("href", "css/tema-oscuro.css");
         logo.setAttribute("src", "assets/general/vector_1_2.png");
@@ -19,6 +17,8 @@ themeButton.addEventListener("click", () => {
         signature.setAttribute("src", "assets/general/signature_1.png");
         imgFilter("");
     }
+
+    themeButton.classList.toggle("active");
 });
 
 function imgFilter(theme) {
