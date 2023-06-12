7b0c95b306e0cf6e4e67275fc7f2d802506d2220
Isaac Vega <skylinecar64@gmail.com>
2023-06-12 14:23:43 -0600

    Actualizando scripts

--- js/index.js
+++ js/index.js
@@ -3,17 +3,14 @@ import { changeTheme } from "./temas.js";
 import { validarInput, valueTextArea, sendForm } from "./validacion.js";
 import { cerrarModal, mostrarImagenModal } from "./modal.js";
 
-const nombreInput = document.getElementById("nombre");
-const emailInput = document.getElementById("email");
-const asuntoInput = document.getElementById("asunto");
-const mensaje = document.getElementById("mensaje");
-const formulario = document.getElementById("formulario");
+// Navbar & Tema
+changeTheme();
+navActions();
 
+// Experiencia
 const evidenceElements = document.querySelectorAll(".experience__evidence");
-
 evidenceElements.forEach((evidenceElement) => {
     const imgElements = evidenceElement.querySelectorAll("img");
-
     imgElements.forEach((imgE) => {
         imgE.addEventListener("click", () => {
             mostrarImagenModal(imgE);
@@ -21,17 +18,18 @@ evidenceElements.forEach((evidenceElement) => {
     });
 
     const spanElement = evidenceElement.querySelector("span");
-
     spanElement.addEventListener("click", () => {
         cerrarModal();
     });
 });
 
-// Navbar & Tema
-navActions();
-changeTheme();
-
 // Formulario
+const nombreInput = document.getElementById("nombre");
+const emailInput = document.getElementById("email");
+const asuntoInput = document.getElementById("asunto");
+const mensaje = document.getElementById("mensaje");
+const formulario = document.getElementById("formulario");
+
 validarInput(nombreInput, "letras");
 validarInput(emailInput, "email");
 validarInput(asuntoInput, "letras");
